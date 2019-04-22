fad21ad606591bcd4ed7c8ec7fa3083815218f7b
Liton Arefin <litonice09@gmail.com>
2019-04-22 15:35:39 +0600

    edited

---
+++ easy-elementor-addons.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: Easy Elementor Addons
+ * Description: Easy and must have Elementor Addons for WordPress Page Builder
+ * Plugin URI: https://jeweltheme.com
+ * Author: WPDeveloper
+ * Version: 1.0.0
+ * Author URI: https://twitter.com/Litonice11
+ * Text Domain: eea
+ * Domain Path: /languages
+ */
+
