ce40b78ea9bf3d6a6abe930ecefb92c48fff8e84
Liton Arefin <litonice09@gmail.com>
2019-06-11 12:36:27 +0600

    finalizing welcome

---
+++ inc/admin/welcome/how-to-features.php
@@ -0,0 +1,63 @@
+
+
+<div class="master_addons_features">
+	<h2>
+		<?php echo MELA;?>
+		</h3>
+		<p class="textcenter">
+			<?php _e( 'Listed Blocks are available not on Easy Blocks - Gutenberg Blocks Page Builder Plugin. Blocks are being added daily ...', MELA_TD ); ?>
+		</p>
+		<div class="master_addons_feature three-col">
+			<div class="col">
+				<div class="feature_content">
+					<img src="<?php echo MELA_PLUGIN_URL .'/assets/images/feature/alert.svg';?>"/>
+					<h3>
+						<?php _e( 'Alert/Notification Block', MELA_TD ); ?>
+					</h3>
+					<p>
+						<?php _e( 'Need to highlight anything special for your audience? Why not use Alert/ Notification block? There are different type of alert like - success, warning, danger, info, light and dark etc.', MELA_TD ); ?>
+					</p>
+				</div>
+			</div>
+			<div class="col">
+				<div class="feature_content">
+					<img src="<?php echo MELA_PLUGIN_URL .'/assets/images/feature/call-to-action.svg';?>" />
+					<h3><?php _e( 'Call to Action Block', MELA_TD ); ?></h3>
+					<p><?php _e( 'Attractive Highlighted Section for Customers. In the marketing section call to action (CTA) is a button which designed to provoke an immediate response. It helps to increase lead in your niche or strategy.', MELA_TD ); ?></p>
+				</div>
+			</div>
+			<div class="col">
+				<div class="feature_content">
+					<img src="<?php echo MELA_PLUGIN_URL .'/assets/images/feature/cards.svg';?>" />
+					<h3><?php _e( 'Cards Block', MELA_TD ); ?></h3>
+					<p><?php _e( 'In the web world "card" used to height a special part of any webpage with image and content. By default, we have arranged the Card block, by keeping the image at the top and then content.', MELA_TD ); ?></p>
+				</div>
+			</div>
+		</div>
+
+
+		<div class="master_addons_feature">
+			<div class="col">
+				<div class="feature_content">
+					<img src="<?php echo MELA_PLUGIN_URL .'/assets/images/feature/image.svg';?>"/>
+					<h3><?php _e( 'Image & Content Block', MELA_TD ); ?></h3>
+					<p><?php _e( 'Keeping both image and content in the same row is mandatory sometimes. There are two variations of alignment in this block. One is for the left image and another for right image.', MELA_TD ); ?></p>
+				</div>
+			</div>
+			<div class="col">
+				<div class="feature_content">
+					<img src="<?php echo MELA_PLUGIN_URL .'/assets/images/feature/quote.svg';?>" />
+					<h3><?php _e( 'Blockquote Block', MELA_TD ); ?></h3>
+					<p><?php _e( 'Highlighting any Quote or sentence with a name can be done from this block. It has different color variation. You can select the color for the Quote icon or for the text too. LTR and RTL makes unique difference.', MELA_TD ); ?></p>
+				</div>
+			</div>
+			<div class="col">
+				<div class="feature_content">
+					<img src="<?php echo MELA_PLUGIN_URL .'/assets/images/feature/testimonial.svg';?>" />
+					<h3><?php _e( 'Testimonial Block', MELA_TD ); ?></h3>
+					<p><?php _e( 'It\'s a single testimonial block. Use this block to arrange client or user feedback in web pages. We recommend using Photo, Name, Designation, and Content for a better look.', MELA_TD ); ?></p>
+				</div>
+			</div>
+		</div>
+
+</div>
