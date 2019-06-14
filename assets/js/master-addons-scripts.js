51702a8698e2308a96bb712e4118c8619ba00940
Liton Arefin <litonice09@gmail.com>
2019-06-14 17:43:36 +0600

    refactored js file

--- assets/js/master-addons-scripts.js
+++ assets/js/master-addons-scripts.js
@@ -57,54 +57,74 @@
 
     var Master_Addons = {
 
+//
+//         try {
+//         (function($) {
+//
+//         })(jQuery);
+// } catch(e) {
+//         //We can also throw from try block and catch it here
+//         // No Error Show
+//     }
+//
         // Master Addons: Accordion
 
         MA_Accordion:function($scope, $) {
 
-            var $advanced_accordion         = $scope.find(".ma-advanced-accordion").eq(0),
-                // elementSettings             = getElementSettings( $scope ),
-                $accordion_title            = $scope.find(".ma-accordion-tab-title"),
-                $accordion_type             = 'accordion',
-                $accordion_speed            = '3000';
+            try {
+                (function($) {
 
-            // Open default actived tab
-            $accordion_title.each(function(){
-                if ( $(this).hasClass('ma-accordion-tab-active-default') ) {
-                    $(this).addClass('ma-accordion-tab-show ma-accordion-tab-active');
-                    $(this).next().slideDown($accordion_speed)
-                }
-            })
+                    var $advanced_accordion         = $scope.find(".ma-advanced-accordion").eq(0),
+                        // elementSettings             = getElementSettings( $scope ),
+                        $accordion_title            = $scope.find(".ma-accordion-tab-title"),
+                        $accordion_type             = 'accordion',
+                        $accordion_speed            = '3000';
 
-            // Remove multiple click event for nested accordion
-            $accordion_title.unbind("click");
+                    // Open default actived tab
+                    $accordion_title.each(function(){
+                        if ( $(this).hasClass('ma-accordion-tab-active-default') ) {
+                            $(this).addClass('ma-accordion-tab-show ma-accordion-tab-active');
+                            $(this).next().slideDown($accordion_speed)
+                        }
+                    });
 
-            $accordion_title.click(function(e) {
-                e.preventDefault();
+                    // Remove multiple click event for nested accordion
+                    $accordion_title.unbind("click");
 
-                var $this = $(this);
+                    $accordion_title.click(function(e) {
+                        e.preventDefault();
 
-                if ( $accordion_type === 'accordion' ) {
-                    if ( $this.hasClass("ma-accordion-tab-show") ) {
-                        $this.removeClass("ma-accordion-tab-show ma-accordion-tab-active");
-                        $this.next().slideUp($accordion_speed);
-                    } else {
-                        $this.parent().parent().find(".ma-accordion-tab-title").removeClass("ma-accordion-tab-show" +
-                            " ma-accordion-tab-active");
-                        $this.parent().parent().find(".ma-accordion-tab-content").slideUp($accordion_speed);
-                        $this.toggleClass("ma-accordion-tab-show ma-accordion-tab-active");
-                        $this.next().slideToggle($accordion_speed);
-                    }
-                } else {
-                    // For acccordion type 'toggle'
-                    if ( $this.hasClass("ma-accordion-tab-show") ) {
-                        $this.removeClass("ma-accordion-tab-show ma-accordion-tab-active");
-                        $this.next().slideUp($accordion_speed);
-                    } else {
-                        $this.addClass("ma-accordion-tab-show ma-accordion-tab-active");
-                        $this.next().slideDown($accordion_speed);
-                    }
-                }
-            });
+                        var $this = $(this);
+
+                        if ( $accordion_type === 'accordion' ) {
+                            if ( $this.hasClass("ma-accordion-tab-show") ) {
+                                $this.removeClass("ma-accordion-tab-show ma-accordion-tab-active");
+                                $this.next().slideUp($accordion_speed);
+                            } else {
+                                $this.parent().parent().find(".ma-accordion-tab-title").removeClass("ma-accordion-tab-show" +
+                                    " ma-accordion-tab-active");
+                                $this.parent().parent().find(".ma-accordion-tab-content").slideUp($accordion_speed);
+                                $this.toggleClass("ma-accordion-tab-show ma-accordion-tab-active");
+                                $this.next().slideToggle($accordion_speed);
+                            }
+                        } else {
+                            // For acccordion type 'toggle'
+                            if ( $this.hasClass("ma-accordion-tab-show") ) {
+                                $this.removeClass("ma-accordion-tab-show ma-accordion-tab-active");
+                                $this.next().slideUp($accordion_speed);
+                            } else {
+                                $this.addClass("ma-accordion-tab-show ma-accordion-tab-active");
+                                $this.next().slideDown($accordion_speed);
+                            }
+                        }
+                    });
+
+                })(jQuery);
+            } catch(e) {
+                //We can also throw from try block and catch it here
+                // No Error Show
+            }
+
 
         },
 
@@ -112,54 +132,76 @@
         // Master Addons: Tabs
 
         MA_Tabs:function($scope, $) {
-            var $tabsWrapper = $scope.find('[data-tabs]').eq(0);
-            $tabsWrapper.each( function() {
-                var tab = $(this);
-                var isTabActive = false;
-                var isContentActive = false;
-                tab.find('[data-tab]').each( function (){
-                    if($(this).hasClass('active')){
-                        isTabActive = true;
-                    }
-                });
-                tab.find('.ma-el-advance-tab-content').each( function (){
-                    if($(this).hasClass('active')){
-                        isContentActive = true;
-                    }
-                });
-                if(!isContentActive){
-                    tab.find('.ma-el-advance-tab-content').eq(0).addClass('active');
-                }
-                if(!isTabActive){
-                    tab.find('[data-tab]').eq(0).addClass('active');
-                }
-                tab.find('[data-tab]').click(function() {
-                    tab.find('[data-tab]').removeClass('active');
-                    tab.find('.ma-el-advance-tab-content').removeClass('active');
-                    $(this).addClass('active');
-                    tab.find('.ma-el-advance-tab-content').eq($(this).index()).addClass('active');
-                });
-            });
+
+            try {
+                (function($) {
+
+                    var $tabsWrapper = $scope.find('[data-tabs]').eq(0);
+                    $tabsWrapper.each( function() {
+                        var tab = $(this);
+                        var isTabActive = false;
+                        var isContentActive = false;
+                        tab.find('[data-tab]').each( function (){
+                            if($(this).hasClass('active')){
+                                isTabActive = true;
+                            }
+                        });
+                        tab.find('.ma-el-advance-tab-content').each( function (){
+                            if($(this).hasClass('active')){
+                                isContentActive = true;
+                            }
+                        });
+                        if(!isContentActive){
+                            tab.find('.ma-el-advance-tab-content').eq(0).addClass('active');
+                        }
+                        if(!isTabActive){
+                            tab.find('[data-tab]').eq(0).addClass('active');
+                        }
+                        tab.find('[data-tab]').click(function() {
+                            tab.find('[data-tab]').removeClass('active');
+                            tab.find('.ma-el-advance-tab-content').removeClass('active');
+                            $(this).addClass('active');
+                            tab.find('.ma-el-advance-tab-content').eq($(this).index()).addClass('active');
+                        });
+                    });
+
+                })(jQuery);
+            } catch(e) {
+                    //We can also throw from try block and catch it here
+                    // No Error Show
+            }
+
+
         },
 
 
         //Master Addons: Progressbar
         ProgressBar: function ($scope, $){
-            var $progressBarWrapper = $scope.find('[data-progress-bar]').eq(0);
-            $progressBarWrapper.waypoint(function () {
-                var element = $(this.element);
-                var id = element.data('id');
-                var type = element.data('type');
-                var value = element.data('progress-bar-value');
-                var strokeWidth = element.data('progress-bar-stroke-width');
-                var strokeTrailWidth = element.data('progress-bar-stroke-trail-width');
-                var color = element.data('stroke-color');
-                var trailColor = element.data('stroke-trail-color');
-                animatedProgressbar(id, type, value, color, trailColor, strokeWidth, strokeTrailWidth);
-                this.destroy();
-            }, {
-                offset: 'bottom-in-view'
-            });
+
+            try {
+                (function($) {
+                    var $progressBarWrapper = $scope.find('[data-progress-bar]').eq(0);
+                    $progressBarWrapper.waypoint(function () {
+                        var element = $(this.element);
+                        var id = element.data('id');
+                        var type = element.data('type');
+                        var value = element.data('progress-bar-value');
+                        var strokeWidth = element.data('progress-bar-stroke-width');
+                        var strokeTrailWidth = element.data('progress-bar-stroke-trail-width');
+                        var color = element.data('stroke-color');
+                        var trailColor = element.data('stroke-trail-color');
+                        animatedProgressbar(id, type, value, color, trailColor, strokeWidth, strokeTrailWidth);
+                        this.destroy();
+                    }, {
+                        offset: 'bottom-in-view'
+                    });
+                })(jQuery);
+            } catch(e) {
+                //We can also throw from try block and catch it here
+                // No Error Show
+            }
+
+
         },
 
         TeamSlider: function ($scope, $){
@@ -189,7 +231,8 @@
                     });
 
                 })(jQuery);
-            } catch(e) {  //We can also throw from try block and catch it here
+            } catch(e) {
+                //We can also throw from try block and catch it here
                 // No Error Show
             }
 
