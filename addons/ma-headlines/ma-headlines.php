3128269232e4b380cf87611e9757f03762013470
Liton Arefin <litonice09@gmail.com>
2019-06-15 10:54:18 +0600

    MA Headlines done

--- addons/ma-headlines/ma-headlines.php
+++ addons/ma-headlines/ma-headlines.php
@@ -25,17 +25,17 @@
 		protected function _register_controls() {
 
 			/**
-			 * Dual Heading Content Section
+			 * Master Headlines Content Section
 			 */
 			$this->start_controls_section(
-				'ma_el_dual_heading_content',
+				'ma_el_headlines_content',
 				[
 					'label' => esc_html__( 'Content', MELA_TD ),
 				]
 			);
 
 			$this->add_control(
-				'ma_el_dual_first_heading',
+				'ma_el_headlines_first_heading',
 				[
 					'label' => esc_html__( 'First Heading', MELA_TD ),
 					'type' => Controls_Manager::TEXT,
@@ -43,22 +43,12 @@
 					'default' => esc_html__( 'First', MELA_TD ),
 				]
 			);
-//			$this->add_control(
-//				'ma_el_dual_second_heading',
-//				[
-//					'label' => esc_html__( 'Second Heading', MELA_TD ),
-//					'type' => Controls_Manager::TEXT,
-//					'label_block' => true,
-//					'default' => esc_html__( 'Second', MELA_TD ),
-//				]
-//			);
-
 
 			$repeater = new Repeater();
 
 
 			$repeater->add_control(
-				'ma_el_dual_second_heading',
+				'ma_el_headlines_second_heading',
 				[
 					'label'                 => __( 'More Titles', MELA_TD ),
 					'type'                  => Controls_Manager::TEXT,
@@ -76,62 +66,14 @@
 				[
 					'type'                  => Controls_Manager::REPEATER,
 					'default'               => [
-						[ 'ma_el_dual_second_heading' => esc_html__( 'Minimal Design', MELA_TD ) ],
-						[ 'ma_el_dual_second_heading' => esc_html__( 'Unique Design', MELA_TD ) ],
-						[ 'ma_el_dual_second_heading' => esc_html__( 'Portfolio Template', MELA_TD ) ],
-						[ 'ma_el_dual_second_heading' => esc_html__( 'Modern Design', MELA_TD ) ],
-						[ 'ma_el_dual_second_heading' => esc_html__( 'HTML5 Template', MELA_TD ) ],
+						[ 'ma_el_headlines_second_heading' => esc_html__( 'Minimal Design', MELA_TD ) ],
+						[ 'ma_el_headlines_second_heading' => esc_html__( 'Unique Design', MELA_TD ) ],
+						[ 'ma_el_headlines_second_heading' => esc_html__( 'Portfolio Template', MELA_TD ) ],
+						[ 'ma_el_headlines_second_heading' => esc_html__( 'Modern Design', MELA_TD ) ],
+						[ 'ma_el_headlines_second_heading' => esc_html__( 'HTML5 Template', MELA_TD ) ],
 					],
 					'fields'                => array_values( $repeater->get_controls() ),
-					'title_field'           => '{{ma_el_dual_second_heading}}',
-				]
-			);
-
-
-			$this->add_control(
-				'ma_el_dual_heading_title_link',
-				[
-					'label' => __( 'Heading URL', MELA_TD ),
-					'type' => Controls_Manager::URL,
-					'placeholder' => __( 'https://your-link.com', MELA_TD ),
-					'label_block' => true,
-					'default' => [
-						'url' => '#',
-						'is_external' => true,
-					],
-				]
-			);
-
-			$this->add_control(
-				'ma_el_dual_heading_description',
-				[
-					'label'       => __( 'Sub Heading', MELA_TD ),
-					'type'        => Controls_Manager::TEXTAREA,
-					'label_block' => true,
-					'dynamic'     => [ 'active' => true ],
-					'default'     => __( 'Add your sub heading here.', MELA_TD ),
-				]
-			);
-
-			$this->add_control(
-				'ma_el_dual_heading_icon_show',
-				[
-					'label' => esc_html__( 'Enable Icon', MELA_TD ),
-					'type' => Controls_Manager::SWITCHER,
-					'default' => 'yes',
-					'return_value' => 'yes',
-				]
-			);
-
-			$this->add_control(
-				'ma_el_dual_heading_icon',
-				[
-					'label'     => __( 'Icon', MELA_TD ),
-					'type'      => Controls_Manager::ICON,
-					'default'   => 'fa fa-wordpress',
-					'condition' => [
-						'ma_el_dual_heading_icon_show' => 'yes'
-					]
+					'title_field'           => '{{ma_el_headlines_second_heading}}',
 				]
 			);
 
@@ -139,64 +81,12 @@
 			$this->end_controls_section();
 
 
-			/*
-			* Dual Heading Styling Section
-			*/
-			$this->start_controls_section(
-				'ma_el_dual_heading_styles_general',
-				[
-					'label' => esc_html__( 'General Styles', MELA_TD ),
-					'tab' => Controls_Manager::TAB_STYLE
-				]
-			);
-
-			$this->add_control(
-				'ma_el_dual_heading_icon_color',
-				[
-					'label'		=> esc_html__( 'Icon Color', MELA_TD ),
-					'type'		=> Controls_Manager::COLOR,
-					'default' => '#132C47',
-					'selectors'	=> [
-						'{{WRAPPER}} .ma-el-dual-heading .ma-el-dual-heading-wrapper .ma-el-dual-heading-icon' => 'color: {{VALUE}};',
-					],
-				]
-			);
-
-			$this->add_responsive_control(
-				'ma_el_dual_heading_alignment',
-				[
-					'label' => esc_html__( 'Alignment', MELA_TD ),
-					'type' => Controls_Manager::CHOOSE,
-					'label_block' => true,
-					'options' => [
-						'left' => [
-							'title' => esc_html__( 'Left', MELA_TD ),
-							'icon' => 'fa fa-align-left',
-						],
-						'center' => [
-							'title' => esc_html__( 'Center', MELA_TD ),
-							'icon' => 'fa fa-align-center',
-						],
-						'right' => [
-							'title' => esc_html__( 'Right', MELA_TD ),
-							'icon' => 'fa fa-align-right',
-						],
-					],
-					'default' => 'center',
-					'label_block' => true,
-					'selectors' => [
-						'{{WRAPPER}} .ma-el-dual-heading .ma-el-dual-heading-wrapper' => 'text-align: {{VALUE}};',
-					],
-				]
-			);
-
-			$this->end_controls_section();
 
 			/*
-				* Dual Heading First Part Styling Section
+				* Master Headlines First Part Styling Section
 				*/
 			$this->start_controls_section(
-				'ma_el_dual_first_heading_styles',
+				'ma_el_headlines_first_heading_styles',
 				[
 					'label' => esc_html__( 'First Heading', MELA_TD ),
 					'tab' => Controls_Manager::TAB_STYLE,
@@ -204,26 +94,26 @@
 			);
 
 			$this->add_control(
-				'ma_el_dual_heading_first_text_color',
+				'ma_el_headlines_first_text_color',
 				[
 					'label'		=> esc_html__( 'Text Color', MELA_TD ),
 					'type'		=> Controls_Manager::COLOR,
 					'default' => '#ffffff',
 					'selectors'	=> [
-						'{{WRAPPER}} .ma-el-dual-heading .ma-el-dual-heading-wrapper .ma-el-dual-heading-title a .first-heading'
+						'{{WRAPPER}} .ma-el-dual-heading .ma-el-dual-heading-wrapper .ma-el-dual-heading-title .first-heading'
 						=> 'color: {{VALUE}};',
 					],
 				]
 			);
 
 			$this->add_control(
-				'ma_el_dual_heading_first_bg_color',
+				'ma_el_headlines_first_bg_color',
 				[
 					'label' => __( 'Background', MELA_TD ),
 					'type' => Controls_Manager::COLOR,
 					'default' => '#704aff',
 					'selectors' => [
-						'{{WRAPPER}} .ma-el-dual-heading .ma-el-dual-heading-wrapper .ma-el-dual-heading-title a .first-heading'
+						'{{WRAPPER}} .ma-el-dual-heading .ma-el-dual-heading-wrapper .ma-el-dual-heading-title .first-heading'
 						=> 'background-color: {{VALUE}};',
 					],
 				]
@@ -232,19 +122,19 @@
 			$this->add_group_control(
 				Group_Control_Typography::get_type(),
 				[
-					'name' => 'ma_el_dual_first_heading_typography',
+					'name' => 'ma_el_headlines_first_heading_typography',
 					'scheme' => Scheme_Typography::TYPOGRAPHY_1,
-					'selector' => '{{WRAPPER}} .ma-el-dual-heading .ma-el-dual-heading-wrapper .ma-el-dual-heading-title a .first-heading',
+					'selector' => '{{WRAPPER}} .ma-el-dual-heading .ma-el-dual-heading-wrapper .ma-el-dual-heading-title .first-heading',
 				]
 			);
 
 			$this->end_controls_section();
 
 			/*
-			* Dual Heading Second Part Styling Section
+			* Master Headlines Second Part Styling Section
 			*/
 			$this->start_controls_section(
-				'ma_el_dual_second_heading_styles',
+				'ma_el_headlines_second_heading_styles',
 				[
 					'label' => esc_html__( 'Second Heading', MELA_TD ),
 					'tab' => Controls_Manager::TAB_STYLE
@@ -252,25 +142,25 @@
 			);
 
 			$this->add_control(
-				'ma_el_dual_heading_second_text_color',
+				'ma_el_headlines_second_text_color',
 				[
 					'label'		=> esc_html__( 'Text Color', MELA_TD ),
 					'type'		=> Controls_Manager::COLOR,
 					'default' => '#132C47',
 					'selectors'	=> [
-						'{{WRAPPER}} .ma-el-dual-heading .ma-el-dual-heading-wrapper .ma-el-dual-heading-title a .second-heading' =>
+						'{{WRAPPER}} .ma-el-dual-heading .ma-el-dual-heading-wrapper .ma-el-dual-heading-title .second-heading' =>
 							'color: {{VALUE}};',
 					],
 				]
 			);
 
 			$this->add_control(
-				'ma_el_dual_heading_second_bg_color',
+				'ma_el_headlines_second_bg_color',
 				[
 					'label' => __( 'Background', MELA_TD ),
 					'type' => Controls_Manager::COLOR,
 					'selectors' => [
-						'{{WRAPPER}} .ma-el-dual-heading .ma-el-dual-heading-wrapper .ma-el-dual-heading-title a .second-heading' =>
+						'{{WRAPPER}} .ma-el-dual-heading .ma-el-dual-heading-wrapper .ma-el-dual-heading-title .second-heading' =>
 							'background-color: {{VALUE}};',
 					],
 				]
@@ -279,124 +169,61 @@
 			$this->add_group_control(
 				Group_Control_Typography::get_type(),
 				[
-					'name' => 'ma_el_dual_second_heading_typography',
+					'name' => 'ma_el_headlines_second_heading_typography',
 					'scheme' => Scheme_Typography::TYPOGRAPHY_1,
-					'selector' => '{{WRAPPER}} .ma-el-dual-heading .ma-el-dual-heading-wrapper .ma-el-dual-heading-title a .second-heading',
+					'selector' => '{{WRAPPER}} .ma-el-dual-heading .ma-el-dual-heading-wrapper .ma-el-dual-heading-title .second-heading',
 				]
 			);
 
 
 			$this->end_controls_section();
 
-			/*
-				* Dual Heading description Styling Section
-			*/
-			$this->start_controls_section(
-				'ma_el_dual_heading_description_styles',
-				[
-					'label' => esc_html__( 'Sub Heading', MELA_TD ),
-					'tab' => Controls_Manager::TAB_STYLE
-				]
-			);
 
-			$this->add_control(
-				'ma_el_dual_heading_description_text_color',
-				[
-					'label'		=> esc_html__( 'Text Color', MELA_TD ),
-					'type'		=> Controls_Manager::COLOR,
-					'default' => '#989B9E',
-					'selectors'	=> [
-						'{{WRAPPER}} .ma-el-dual-heading .ma-el-dual-heading-wrapper .ma-el-dual-heading-description' =>
-							'color: {{VALUE}};',
-					],
-				]
-			);
-			$this->add_group_control(
-				Group_Control_Typography::get_type(),
-				[
-					'name' => 'ma_el_dual_heading_description_typography',
-					'scheme' => Scheme_Typography::TYPOGRAPHY_1,
-					'selector' => '{{WRAPPER}} .ma-el-dual-heading .ma-el-dual-heading-wrapper .ma-el-dual-heading-description',
-				]
-			);
-
-			$this->end_controls_section();
 		}
 
 		protected function render() {
 			$settings = $this->get_settings_for_display();
-
-			foreach( $settings['tabs'] as $index => $tab ) {
-
-//
-//				$tab_count = $index+1;
-//				$tab_title_setting_key = $this->get_repeater_setting_key('ma_el_dual_second_heading', 'tabs', $index);
-////				$tab_content_setting_key = $this->get_repeater_setting_key('accordion_content', 'tabs', $index);
-//
-//				$tab_title_class 	= ['ma-accordion-tab-title'];
-//				$tab_content_class 	= ['ma-accordion-tab-content'];
-//
-//				if ( $tab['accordion_tab_default_active'] == 'yes' ) {
-//					$tab_title_class[] 		= 'ma-accordion-tab-active-default';
-//					$tab_content_class[] 	= 'ma-accordion-tab-active-default';
-//				}
-?>
-
-				<h1 class="cd-headline rotate-1 main-title">
-					<span>Mama - The Best</span>
-						<span class="cd-words-wrapper">
-				        <b class="is-visible">Minimal Template</b>
-				        <b><?php echo $tab['tab_title']; ?></b>
-				    </span>
-				</h1>
-<?php
-
-
-			}
-
 			?>
 
-			<div id="ma-el-heading-<?php echo esc_attr($this->get_id()); ?>" class="ma-el-dual-heading">
-				<div class="ma-el-dual-heading-wrapper">
-					<?php if ( $settings['ma_el_dual_heading_icon_show'] == 'yes' ) : ?>
-						<span class="ma-el-dual-heading-icon"><i class="<?php echo esc_attr( $settings['ma_el_dual_heading_icon'] ); ?>"></i></span>
-					<?php endif; ?>
-					<h1 class="ma-el-dual-heading-title">
-						<a href="<?php echo esc_url( $settings['ma_el_dual_heading_title_link']['url'] ); ?>">
-							<span class="first-heading"><?php echo esc_html( $settings['ma_el_dual_first_heading'] );
-								?></span><span class="second-heading"><?php //echo esc_html(
-									// $settings['ma_el_dual_second_heading'] ); ?></span>
-						</a>
-					</h1>
-					<?php if ( $settings['ma_el_dual_heading_description'] != "" ) : ?>
-						<p class="ma-el-dual-heading-description"><?php echo esc_html( $settings['ma_el_dual_heading_description'] ); ?></p>
-					<?php endif; ?>
-				</div>
-			</div>
+                <div id="ma-el-heading-<?php echo esc_attr($this->get_id()); ?>" class="ma-el-dual-heading">
+                    <div class="ma-el-dual-heading-wrapper">
+
+                        <h1 class="ma-el-dual-heading-title cd-headline rotate-1 main-title">
+                            <span class="first-heading">
+                                <?php echo esc_html( $settings['ma_el_headlines_first_heading'] ); ?>
+                            </span>
+                            <span class="cd-words-wrapper">
+                                <?php foreach( $settings['tabs'] as $index => $tab ) { ?>
+                                    <b class="second-heading <?php echo ($index==0) ? "is-visible": "";?>">
+                                        <?php echo $tab['ma_el_headlines_second_heading']; ?>
+                                    </b>
+                                <?php } ?>
+                            </span>
+                        </h1>
+
+                    </div>
+                </div>
+
 			<?php
 		}
 
-		protected function _content_template() {
-			?>
+
+		protected function _content_template() { ?>
+
 			<div id="ma-el-heading" class="ma-el-dual-heading">
 				<div class="ma-el-dual-heading-wrapper">
-					<# if ( settings.ma_el_dual_heading_icon_show == 'yes' ) { #>
-					<span class="ma-el-dual-heading-icon"><i class="{{ settings.ma_el_dual_heading_icon }}"></i></span>
-					<# } #>
-					<h1 class="ma-el-dual-heading-title">
-						<a href="{{{ settings.ma_el_dual_heading_title_link }}}">
-							<span class="first-heading">{{{ settings.ma_el_dual_first_heading }}}</span><span
-								class="second-heading">{{{ settings.ma_el_dual_second_heading }}}</span>
-						</a>
+
+                    <h1 class="ma-el-dual-heading-title cd-headline rotate-1 main-title">
+                        <span class="first-heading">{{{ settings.ma_el_headlines_first_heading }}}</span>
+                        <# _.each( settings.ma_el_exclusive_tabs, function( tab, index ) { #>
+                            <span class="second-heading">{{{ settings.ma_el_headlines_second_heading }}}</span>
+                        <# }); #>
 					</h1>
-					<# if ( settings.ma_el_dual_heading_description != "" ) { #>
-					<p class="ma-el-dual-heading-description">{{{ settings.ma_el_dual_heading_description }}}</p>
-					<# } #>
+
 				</div>
 			</div>
-			<?php
-		}
 
+        <?php }
 
 	}
 
--- assets/js/master-addons-scripts.js
+++ assets/js/master-addons-scripts.js
@@ -67,8 +67,177 @@
 //         // No Error Show
 //     }
 //
-        // Master Addons: Accordion
+        // Master Addons: Headlines
 
+        MA_Headlines: function($scope, $) {
+            try {
+                (function($) {
+
+                    /*----------- Animated Headlines --------------*/
+
+                    //set animation timing
+                    var animationDelay = 2500,
+                        //loading bar effect
+                        barAnimationDelay = 3800,
+                        barWaiting = barAnimationDelay - 3000, //3000 is the duration of the transition on the loading bar - set in the scss/css file
+                        //letters effect
+                        lettersDelay = 50,
+                        //type effect
+                        typeLettersDelay = 150,
+                        selectionDuration = 500,
+                        typeAnimationDelay = selectionDuration + 800,
+                        //clip effect
+                        revealDuration = 600,
+                        revealAnimationDelay = 1500;
+
+                    initHeadline();
+
+
+                    function initHeadline() {
+                        //insert <i> element for each letter of a changing word
+                        singleLetters($('.cd-headline.letters').find('b'));
+                        //initialise headline animation
+                        animateHeadline($('.cd-headline'));
+                    }
+
+                    function singleLetters($words) {
+                        $words.each(function(){
+                            var word = $(this),
+                                letters = word.text().split(''),
+                                selected = word.hasClass('is-visible');
+                            for (i in letters) {
+                                if(word.parents('.rotate-2').length > 0) letters[i] = '<em>' + letters[i] + '</em>';
+                                letters[i] = (selected) ? '<i class="in">' + letters[i] + '</i>': '<i>' + letters[i] + '</i>';
+                            }
+                            var newLetters = letters.join('');
+                            word.html(newLetters).css('opacity', 1);
+                        });
+                    }
+
+                    function animateHeadline($headlines) {
+                        var duration = animationDelay;
+                        $headlines.each(function(){
+                            var headline = $(this);
+
+                            if(headline.hasClass('loading-bar')) {
+                                duration = barAnimationDelay;
+                                setTimeout(function(){ headline.find('.cd-words-wrapper').addClass('is-loading') }, barWaiting);
+                            } else if (headline.hasClass('clip')){
+                                var spanWrapper = headline.find('.cd-words-wrapper'),
+                                    newWidth = spanWrapper.width() + 10
+                                spanWrapper.css('width', newWidth);
+                            } else if (!headline.hasClass('type') ) {
+                                //assign to .cd-words-wrapper the width of its longest word
+                                var words = headline.find('.cd-words-wrapper b'),
+                                    width = 0;
+                                words.each(function(){
+                                    var wordWidth = $(this).width();
+                                    if (wordWidth > width) width = wordWidth;
+                                });
+                                headline.find('.cd-words-wrapper').css('width', width);
+                            };
+
+                            //trigger animation
+                            setTimeout(function(){ hideWord( headline.find('.is-visible').eq(0) ) }, duration);
+                        });
+                    }
+
+                    function hideWord($word) {
+                        var nextWord = takeNext($word);
+
+                        if($word.parents('.cd-headline').hasClass('type')) {
+                            var parentSpan = $word.parent('.cd-words-wrapper');
+                            parentSpan.addClass('selected').removeClass('waiting');
+                            setTimeout(function(){
+                                parentSpan.removeClass('selected');
+                                $word.removeClass('is-visible').addClass('is-hidden').children('i').removeClass('in').addClass('out');
+                            }, selectionDuration);
+                            setTimeout(function(){ showWord(nextWord, typeLettersDelay) }, typeAnimationDelay);
+
+                        } else if($word.parents('.cd-headline').hasClass('letters')) {
+                            var bool = ($word.children('i').length >= nextWord.children('i').length) ? true : false;
+                            hideLetter($word.find('i').eq(0), $word, bool, lettersDelay);
+                            showLetter(nextWord.find('i').eq(0), nextWord, bool, lettersDelay);
+
+                        }  else if($word.parents('.cd-headline').hasClass('clip')) {
+                            $word.parents('.cd-words-wrapper').animate({ width : '2px' }, revealDuration, function(){
+                                switchWord($word, nextWord);
+                                showWord(nextWord);
+                            });
+
+                        } else if ($word.parents('.cd-headline').hasClass('loading-bar')){
+                            $word.parents('.cd-words-wrapper').removeClass('is-loading');
+                            switchWord($word, nextWord);
+                            setTimeout(function(){ hideWord(nextWord) }, barAnimationDelay);
+                            setTimeout(function(){ $word.parents('.cd-words-wrapper').addClass('is-loading') }, barWaiting);
+
+                        } else {
+                            switchWord($word, nextWord);
+                            setTimeout(function(){ hideWord(nextWord) }, animationDelay);
+                        }
+                    }
+
+                    function showWord($word, $duration) {
+                        if($word.parents('.cd-headline').hasClass('type')) {
+                            showLetter($word.find('i').eq(0), $word, false, $duration);
+                            $word.addClass('is-visible').removeClass('is-hidden');
+
+                        }  else if($word.parents('.cd-headline').hasClass('clip')) {
+                            $word.parents('.cd-words-wrapper').animate({ 'width' : $word.width() + 10 }, revealDuration, function(){
+                                setTimeout(function(){ hideWord($word) }, revealAnimationDelay);
+                            });
+                        }
+                    }
+
+                    function hideLetter($letter, $word, $bool, $duration) {
+                        $letter.removeClass('in').addClass('out');
+
+                        if(!$letter.is(':last-child')) {
+                            setTimeout(function(){ hideLetter($letter.next(), $word, $bool, $duration); }, $duration);
+                        } else if($bool) {
+                            setTimeout(function(){ hideWord(takeNext($word)) }, animationDelay);
+                        }
+
+                        if($letter.is(':last-child') && $('html').hasClass('no-csstransitions')) {
+                            var nextWord = takeNext($word);
+                            switchWord($word, nextWord);
+                        }
+                    }
+
+                    function showLetter($letter, $word, $bool, $duration) {
+                        $letter.addClass('in').removeClass('out');
+
+                        if(!$letter.is(':last-child')) {
+                            setTimeout(function(){ showLetter($letter.next(), $word, $bool, $duration); }, $duration);
+                        } else {
+                            if($word.parents('.cd-headline').hasClass('type')) { setTimeout(function(){ $word.parents('.cd-words-wrapper').addClass('waiting'); }, 200);}
+                            if(!$bool) { setTimeout(function(){ hideWord($word) }, animationDelay) }
+                        }
+                    }
+
+                    function takeNext($word) {
+                        return (!$word.is(':last-child')) ? $word.next() : $word.parent().children().eq(0);
+                    }
+
+                    function takePrev($word) {
+                        return (!$word.is(':first-child')) ? $word.prev() : $word.parent().children().last();
+                    }
+
+                    function switchWord($oldWord, $newWord) {
+                        $oldWord.removeClass('is-visible').addClass('is-hidden');
+                        $newWord.removeClass('is-hidden').addClass('is-visible');
+                    }
+
+
+                })(jQuery);
+            } catch(e) {
+                        //We can also throw from try block and catch it here
+                        // No Error Show
+            }
+
+        },
+
+        // Master Addons: Accordion
         MA_Accordion:function($scope, $) {
 
             try {
@@ -209,28 +378,6 @@
             try {
                 (function($) {
 
-                    // var teamSlider = $("#ma-el-team-member-slider");
-                    //
-                    // teamSlider.owlCarousel({
-                    //     autoPlay : 3000,
-                    //     stopOnHover : true,
-                    //     pagination : true,
-                    //     paginationNumbers: false,
-                    //
-                    //     itemsCustom : [
-                    //         [0, 1],
-                    //         [450, 1],
-                    //         [600, 1],
-                    //         [700, 2],
-                    //         [1000, 3],
-                    //         [1200, 4],
-                    //     ],
-                    //     // Responsive
-                    //     responsive: true,
-                    //     responsiveRefreshRate : 200,
-                    //     responsiveBaseWidth: window
-                    // });
-
                     var $teamCarouselWrapper = $scope.find('.ma-el-team-carousel-wrapper').eq(0),
                         $carousel_nav = $teamCarouselWrapper.data("carousel-nav"),
                         $loop = ($teamCarouselWrapper.data("loop") !== undefined) ? $teamCarouselWrapper.data("loop") : false,
@@ -300,6 +447,7 @@
         if( elementorFrontend.isEditMode() ) {
             editMode = true;
         }
+        elementorFrontend.hooks.addAction('frontend/element_ready/ma-headlines.default', Master_Addons.MA_Headlines);
         elementorFrontend.hooks.addAction('frontend/element_ready/ma-advanced-accordion.default', Master_Addons.MA_Accordion);
         elementorFrontend.hooks.addAction('frontend/element_ready/ma-tabs.default', Master_Addons.MA_Tabs);
         elementorFrontend.hooks.addAction('frontend/element_ready/ma-progressbar.default', Master_Addons.ProgressBar);
