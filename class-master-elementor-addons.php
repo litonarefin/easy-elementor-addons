de4ca745cc72031f03f4ff9ef2c337792ff51d69
Liton Arefin <litonice09@gmail.com>
2019-07-23 02:36:30 +0600

    reverted

--- class-master-elementor-addons.php
+++ class-master-elementor-addons.php
@@ -1,24 +1,11 @@
 <?php
- /**
- * Plugin Name: Master Addons for Elementor
- * Description: Master Addons is easy and must have Elementor Addons for WordPress Page Builder. Clean, Modern, Hand crafted designed Addons blocks.
- * Plugin URI: https://wordpress.org/plugins/master-addons
- * Author: Jewel Theme
- * Version: 1.0.7
- * Author URI: https://twitter.com/Litonice11
- * Text Domain: mela
- * Domain Path: /languages
- */
 
 	if (!defined('ABSPATH')) { exit; } // No, Direct access Sir !!!
 
-<<<<<<< HEAD
-
 	if( !class_exists('Master_Elementor_Addons') ){
-
 		final class Master_Elementor_Addons {
 
-			const VERSION = "1.0.7";
+			const VERSION = "1.0.5";
 
 			const MINIMUM_PHP_VERSION = '5.4';
 
@@ -35,11 +22,6 @@
 			private static $instance = null;
 
 			public $pro_enabled;
-
-			/**
-			 * @var Freemius
-			 */
-			public $fs;
 
 			public static $maad_el_default_widgets;
 			public static $maad_el_default_form_widgets;
@@ -155,193 +137,9 @@
 				$this->mela_load_textdomain();
 				$this->ma_el_image_size();
 
-				// Init Freemius.
-				$this->ma_el_fs();
-				$this->ma_el_freemius_hooks();
-				// Signal that SDK was initiated.
-				do_action( 'ma_el_fs_loaded' );
-
 				//Redirect Hook
 				add_action( 'admin_init', [ $this, 'mael_ad_redirect_hook' ]);
 
-			}
-
-
-
-			// Create a helper function for easy SDK access.
-			public function ma_el_fs() {
-				global $ma_el_fs;
-
-				if ( ! isset( $ma_el_fs ) ) {
-					// Activate multisite network integration.
-					if ( ! defined( 'WP_FS__PRODUCT_4015_MULTISITE' ) ) {
-						define( 'WP_FS__PRODUCT_4015_MULTISITE', true );
-					}
-
-					// Include Freemius SDK.
-					require_once dirname(__FILE__) . '/lib/freemius/start.php';
-
-					$ma_el_fs = fs_dynamic_init( array(
-						'id'                  => MA_EL_FREEMIUS_ID,
-						'slug'                => 'master-addons',
-						'type'                => 'plugin',
-						'public_key'          => MA_EL_FREEMIUS_PUBLIC_KEY,
-						'is_premium'          => true,
-						'is_premium_only'     => true,
-						'has_addons'          => false,
-						'has_paid_plans'      => true,
-						'trial'               => array(
-							'days'               => 14,
-							'is_require_payment' => false,
-						),
-						'menu'                => array(
-							'slug'           => 'master-addons-settings',
-							'first-path'     => 'admin.php?page=master-addons-settings',
-						),
-						// Set the SDK to work in a sandbox mode (for development & testing).
-						// IMPORTANT: MAKE SURE TO REMOVE SECRET KEY BEFORE DEPLOYMENT.
-						'secret_key'          => MA_EL_FREEMIUS_SECRET_KEY,
-					) );
-
-				}
-
-				return $ma_el_fs;
-			}
-
-            function ma_el_freemius_hooks(){
-
-	            $fs= $this->ma_el_fs();
-
-	            $fs->add_filter( 'show_first_trial_after_n_sec', [$this, 'ma_el_fs_show_first_trial_after_7_days']);
-
-	            $fs->add_filter( 'reshow_trial_after_every_n_sec', [$this,'ma_el_fs_reshow_trial_after_every_60_days']);
-
-	            // Trial
-	            $fs->override_i18n( array(
-		            'hey'                                        => 'Hey',
-		            'trial-x-promotion-message'                  => 'Thank you so much for using %s!',
-		            'already-opted-in-to-product-usage-tracking' => 'How do you like %s so far? Test all our %s premium features with a %d-day free trial.',
-		            'start-free-trial'                           => 'Start free trial',
-		            // Trial with a payment method required.
-		            'no-commitment-for-x-days'                   => 'No commitment for %s days - cancel anytime!',
-		            // Trial without a payment method.
-		            'no-cc-required'                             => 'No credit card required',
-	            ) );
-
-
-                // Freemius submenu items visibility filter
-	            $fs->add_filter( 'is_submenu_visible', [$this, 'ma_el_fs_is_submenu_visible'], 10, 2 );
-
-	            $fs->add_filter('checkout/purchaseCompleted', [$this, 'ma_el_fs_after_purchase_js' ]);
-
-	            $fs->add_filter( 'show_admin_notice', [$this, 'ma_el_fs_custom_show_admin_notice'], 10, 2 );
-
-
-	            $fs->add_filter( 'permissions_list', [$this, 'ma_el_fs_add_helpscount_permission' ]);
-
-
-	            $fs->add_filter('connect_message_on_update', [$this, 'ma_el_fs_custom_connect_message_on_update'],
-                    10, 6);
-
-//	             Not like register_uninstall_hook(), you do NOT have to use a static function.
-	            $fs->add_action('after_uninstall', [$this, 'ma_el_fs_uninstall_cleanup']);
-
-	            add_action( 'wp_head', [$this,'ma_el_fs_add_licensing_helper' ]);
-
-
-            }
-
-			function ma_el_fs_add_licensing_helper() { ?>
-                <script type="text/javascript">
-                    (function () {
-                        window.ma_el_fs = { can_use_premium_code: <?php
-							echo  json_encode( ma_el_fs()->can_use_premium_code() ) ;
-							?>};
-                    })();
-                </script>
-				<?php
-			}
-
-
-			// Customize Opt-in Message for Existing Users
-			function ma_el_fs_custom_connect_message_on_update(
-				$message,
-				$user_first_name,
-				$plugin_title,
-				$user_login,
-				$site_link,
-				$freemius_link
-			) {
-				return sprintf(
-					__( 'Hey %1$s' ) . ',<br>' .
-					__( 'Please help us improve %2$s! If you opt-in, some data about your usage of %2$s will be sent to %5$s. If you skip this, that\'s okay! %2$s will still work just fine.', 'master-addons' ),
-					$user_first_name,
-					'<b>' . $plugin_title . '</b>',
-					'<b>' . $user_login . '</b>',
-					$site_link,
-					$freemius_link
-				);
-			}
-
-
-			function ma_el_fs_add_helpscount_permission( $permissions ) {
-				$permissions['helpscout'] = array(
-					'icon-class' => 'dashicons dashicons-email-alt',
-					'label'      => ma_el_fs()->get_text_inline( 'Help Scout', 'helpscout' ),
-					'desc'       => ma_el_fs()->get_text_inline( 'Rendering Help Scout\'s beacon for easy support access', 'permissions-helpscout' ),
-					'priority'   => 16,
-				);
-
-				$permissions['newsletter'] = array(
-					'icon-class' => 'dashicons dashicons-email-alt',
-					'label'      => ma_el_fs()->get_text_inline( 'Newsletter', 'permissions-newsletter' ),
-					'desc'       => ma_el_fs()->get_text_inline( 'Updates, announcements, marketing, no spam', 'permissions-newsletter_desc' ),
-					'priority'   => 15,
-				);
-
-			}
-
-
-			function ma_el_fs_custom_show_admin_notice( $show, $msg ) {
-				if ('trial_promotion' == $msg['id']) {
-					// Don't show the trial promotional admin notice.
-					return false;
-				}
-
-				return $show;
-			}
-
-			// Freemius Purchase Completion JavaScript Callback Filter
-			function ma_el_fs_after_purchase_js( $js_function ) {
-				return 'function (data) {
-                    console.log("checkout", "purchaseCompleted");
-                }';
-			}
-
-
-			// Freemius submenu items visibility filter
-			function ma_el_fs_is_submenu_visible($is_visible, $submenu_id){
-				return $is_visible;
-			}
-
-
-			#----------------------------------------------------------------------------------
-			#region Re-show the trial promotional offer after every 60 days instead of 30 days.
-			#----------------------------------------------------------------------------------
-
-			function ma_el_fs_reshow_trial_after_every_60_days( $thirty_days_in_sec ) {
-				// 60 days in sec.
-				return 60 * 24 * 60 * 60;
-			}
-
-
-			#----------------------------------------------------------------------------------
-			#region Show the 1st trial promotion after 7 days instead of 24 hours.
-			#----------------------------------------------------------------------------------
-
-			function ma_el_fs_show_first_trial_after_7_days( $day_in_sec ) {
-				// 7 days in sec.
-				return 7 * 24 * 60 * 60;
 			}
 
 
@@ -427,22 +225,6 @@
 
 				if ( ! defined( 'MELA_DIR' ) ) {
 					define( 'MELA_DIR', dirname( __FILE__ ) );
-				}
-
-				/*
-				 * Freemius
-				 */
-
-				if ( ! defined( 'MA_EL_FREEMIUS_ID' ) ) {
-					define( 'MA_EL_FREEMIUS_ID', '4015' );
-				}
-
-				if ( ! defined( 'MA_EL_FREEMIUS_PUBLIC_KEY' ) ) {
-					define( 'MA_EL_FREEMIUS_PUBLIC_KEY', 'pk_3c9b5b4e47a06288e3500c7bf812e' );
-				}
-
-				if ( ! defined( 'MA_EL_FREEMIUS_SECRET_KEY' ) ) {
-					define( 'MA_EL_FREEMIUS_SECRET_KEY', 'sk_3AARVId*C!kTXIUeYO0Cq>w%Vl?d@' );
 				}
 
 			}
@@ -848,85 +630,8 @@
 
 
 
-=======
-    
-	if ( ! function_exists( 'ma_el_fs' ) ) {
-		// Create a helper function for easy SDK access.
-		function ma_el_fs() {
-			global $ma_el_fs;
-
-			if ( ! isset( $ma_el_fs ) ) {
-				// Activate multisite network integration.
-				if ( ! defined( 'WP_FS__PRODUCT_4015_MULTISITE' ) ) {
-					define( 'WP_FS__PRODUCT_4015_MULTISITE', true );
-				}
-
-				// Include Freemius SDK.
-				require_once dirname(__FILE__) . '/lib/freemius/start.php';
-
-				$ma_el_fs = fs_dynamic_init( array(
-					'id'                  => '4015',
-					'slug'                => 'master-addons',
-					'type'                => 'plugin',
-					'public_key'          => 'pk_3c9b5b4e47a06288e3500c7bf812e',
-					'is_premium'          => false,
-					'has_addons'          => false,
-					'has_paid_plans'      => false,
-					'menu'                => array(
-						'slug'           => 'master-addons-settings',
-						'support'        => false,
-						'first-path'     => 'admin.php?page=master-addons-settings',
-						'account'        => false,
-					),
-					'is_live'        => true,
-				) );
-			}
-
-			return $ma_el_fs;
->>>>>>> parent of e0b27a4... finalized freemius codes
 		}
 
 
 	}
 
-	Master_Elementor_Addons::get_instance();
-
-
-	/**
-	 * Plugin Redirect Option Added by register_activation_hook
-	 *
-	 */
-	function master_addons_el_redirect() {
-		add_option( 'maad_el_update_redirect', true );
-	}
-	register_activation_hook( __FILE__ , 'master_addons_el_redirect' );
-
-
-<<<<<<< HEAD
-=======
-
-	//Instantiate Master Addons Class
-	require_once dirname( __FILE__ ) . '/class-master-elementor-addons.php';
-	Master_Elementor_Addons::get_instance();
-
-
-	/**
-	 * Plugin Redirect Option Added by register_activation_hook
-	 *
-	 */
-	function master_addons_el_redirect() {
-		add_option( 'maad_el_update_redirect', true );
-	}
-	register_activation_hook( __FILE__ , 'master_addons_el_redirect' );
-
-
->>>>>>> parent of e0b27a4... finalized freemius codes
-	// Deactivation Hook
-	function master_addons_el_welcome_deactivate() {
-		delete_transient( 'maad_el_update_redirect' );
-	}
-<<<<<<< HEAD
-	register_deactivation_hook( __FILE__, 'master_addons_el_welcome_deactivate' );
-=======
-	register_deactivation_hook( __FILE__, 'master_addons_el_welcome_deactivate' );
->>>>>>> parent of e0b27a4... finalized freemius codes
