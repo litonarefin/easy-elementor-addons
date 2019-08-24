e6b4aa56638ab48cbc6a7d1ceb08087df954dac1
Liton Arefin <litonice09@gmail.com>
2019-08-24 20:04:50 +0600

    working on infobox style

--- addons/ma-infobox/ma-infobox.php
+++ addons/ma-infobox/ma-infobox.php
@@ -54,6 +54,10 @@
 						'img' => [
 							'title' => esc_html__( 'Image', MELA_TD ),
 							'icon' => 'fa fa-picture-o',
+						],
+						'imgicon' => [
+							'title' => esc_html__( 'Image and Icon', MELA_TD ),
+							'icon' => 'eicon-info-box',
 						]
 					],
 					'default' => 'icon',
@@ -61,6 +65,18 @@
 			);
 
 
+
+			$this->add_control(
+				'ma_el_infobox_icon',
+				[
+					'label' => esc_html__( 'Icon', MELA_TD ),
+					'type' => Controls_Manager::ICON,
+					'default' => 'fa fa-tag',
+					'condition' => [
+						'ma_el_infobox_img_or_icon' => ['icon','imgicon']
+					]
+				]
+			);
 
 			$this->add_control(
 				'ma_el_infobox_image',
@@ -71,7 +87,7 @@
 						'url' => Utils::get_placeholder_image_src(),
 					],
 					'condition' => [
-						'ma_el_infobox_img_or_icon' => 'img'
+						'ma_el_infobox_img_or_icon' => ['img','imgicon']
 					]
 				]
 			);
@@ -81,19 +97,7 @@
 					'name' => 'thumbnail',
 					'default' => 'full',
 					'condition' => [
-						'ma_el_infobox_img_or_icon' => 'img'
-					]
-				]
-			);
-
-			$this->add_control(
-				'ma_el_infobox_icon',
-				[
-					'label' => esc_html__( 'Icon', MELA_TD ),
-					'type' => Controls_Manager::ICON,
-					'default' => 'fa fa-tag',
-					'condition' => [
-						'ma_el_infobox_img_or_icon' => 'icon'
+						'ma_el_infobox_img_or_icon' => ['img','imgicon']
 					]
 				]
 			);
@@ -132,6 +136,38 @@
 				]
 			);
 
+
+			$this->add_control(
+				'ma_el_infobox_readmore_text',
+				[
+					'label' => esc_html__( 'Read More Text', MELA_TD ),
+					'type' => Controls_Manager::TEXT,
+					'label_block' => true,
+					'default' => esc_html__( 'Learn More', MELA_TD ),
+					'condition' => [
+						'ma_el_infobox_preset' => 'six'
+					]
+				]
+			);
+
+
+			$this->add_control(
+				'ma_el_infobox_readmore_link',
+				[
+					'label' => __( 'Read More Link', MELA_TD ),
+					'type' => Controls_Manager::URL,
+					'placeholder' => __( 'https://master-addons.com/demo', MELA_TD ),
+					'label_block' => true,
+					'default' => [
+						'url' => '#',
+						'is_external' => true,
+					],
+					'condition' => [
+						'ma_el_infobox_preset' => 'six'
+					]
+				]
+			);
+
 			$this->end_controls_section();
 
 
@@ -162,8 +198,6 @@
 						'eight' => esc_html__( 'Style 8', MELA_TD ),
 						'nine' => esc_html__( 'Style 9', MELA_TD ),
 						'ten' => esc_html__( 'Style 10', MELA_TD ),
-						'eleven' => esc_html__( 'Style 11', MELA_TD ),
-						'twelve' => esc_html__( 'Style 12', MELA_TD ),
 					],
 				]
 			);
@@ -171,47 +205,117 @@
 			$this->add_control(
 				'ma_el_infobox_color_scheme',
 				[
-					'label' => __('Icon Color Scheme', MELA_TD),
+					'label' => __('Icon Color', MELA_TD),
 					'type' => Controls_Manager::COLOR,
-					'default' => '#704aff',
+					'default' => '#4b00e7',
 					'selectors' => [
 						'{{WRAPPER}} .ma-el-infobox.one .ma-el-infobox-icon::before' => 'background: {{VALUE}};',
-						'{{WRAPPER}} .ma-el-infobox.one .ma-el-infobox-icon i, 
-						{{WRAPPER}} .ma-el-infobox.two .ma-el-infobox-item:hover .ma-el-infobox-icon i, 
-						{{WRAPPER}} .ma-el-infobox.three .ma-el-infobox-item .ma-el-infobox-icon i, 
-						{{WRAPPER}} .ma-el-infobox.four .ma-el-infobox-item:hover .ma-el-infobox-icon i, 
+						'{{WRAPPER}} .ma-el-infobox.one .ma-el-infobox-icon i,
+						{{WRAPPER}} .ma-el-infobox.two .ma-el-infobox-item:hover .ma-el-infobox-icon i,
+						{{WRAPPER}} .ma-el-infobox.three .ma-el-infobox-item .ma-el-infobox-icon i,
+						{{WRAPPER}} .ma-el-infobox.four .ma-el-infobox-item:hover .ma-el-infobox-icon i,
 						{{WRAPPER}} .ma-el-infobox.five .ma-el-infobox-item:hover .ma-el-infobox-icon i' => 'color: {{VALUE}};',
-						'{{WRAPPER}} .ma-el-infobox.one .ma-el-infobox-item:hover .ma-el-infobox-icon i' => 'color: #FFF',
+
+                        '{{WRAPPER}} .ma-el-infobox.one .ma-el-infobox-item:hover .ma-el-infobox-icon i' => 'color: #FFF',
 						'{{WRAPPER}} .ma-el-infobox.two .ma-el-infobox-icon' => 'background-color: {{VALUE}};',
-						'{{WRAPPER}} .ma-el-infobox.two .ma-el-infobox-item:hover .ma-el-infobox-icon, 
-						{{WRAPPER}} .ma-el-infobox.four .ma-el-infobox-item:hover .ma-el-infobox-icon, 
+						'{{WRAPPER}} .ma-el-infobox.two .ma-el-infobox-item:hover .ma-el-infobox-icon,
+						{{WRAPPER}} .ma-el-infobox.four .ma-el-infobox-item:hover .ma-el-infobox-icon,
 						{{WRAPPER}} .ma-el-infobox.five .ma-el-infobox-item:hover .ma-el-infobox-icon' => 'background: #FFF; border: 1px solid {{VALUE}};',
 
 						'{{WRAPPER}} .ma-el-infobox.three .ma-el-infobox-item:hover .ma-el-infobox-icon i' => 'color: #FFF',
-						/*new added line */
 
+						/*new added line */
 						'{{WRAPPER}} .ma-el-infobox.four .ma-el-infobox-icon, {{WRAPPER}} .ma-el-infobox.five .ma-el-infobox-icon' =>
 							'background: {{VALUE}};',
 						'{{WRAPPER}} .ma-el-infobox.five .ma-el-infobox-item' => 'border-bottom: 3px solid {{VALUE}};',
+						'{{WRAPPER}} .ma-el-infobox.six .ma-el-infobox-item' => 'text-align: center};',
+						'{{WRAPPER}} .ma-el-infobox.six .ma-el-infobox-content' => 'text-align: center};',
+						'{{WRAPPER}} .ma-el-infobox.four .ma-el-infobox-item:hover' => 'background-image: linear-gradient( rgb(10,57,252) 0%, rgb(213,0,137) 100%)};'
+
+
 
 					],
+				]
+			);
+
+
+
+			$this->add_control(
+				'ma_el_infobox_bg_heading',
+				[
+					'label' => __( 'Background', MELA_TD ),
+					'type' => Controls_Manager::HEADING,
+				]
+			);
+
+
+			$this->start_controls_tabs( 'ma_el_infobox_style' );
+
+			$this->start_controls_tab(
+				'ma_el_infobox_normal',
+				[
+					'label'                 => __( 'Normal', MELA_TD ),
 				]
 			);
 
 			$this->add_group_control(
 				Group_Control_Background::get_type(),
 				[
-					'name' => 'background',
-					'label' => __( 'Background', MELA_TD ),
+					'name' => 'ma_el_infobox_bg',
+					'label' => __( 'Background Color', MELA_TD ),
 					'types' => [ 'classic', 'gradient' ],
-					'separator' => 'before',
-					'selector' => '{{WRAPPER}} .ma-el-infobox .ma-el-infobox-item',
+					'selector' => '{{WRAPPER}} .ma-el-infobox',
+//					'selectors'	=> [
+//						'{{WRAPPER}} .ma-el-infobox.one',
+//					],
 					'default' => '#FFFFFF',
+				]
+			);
+
+			$this->end_controls_tab();
+
+
+			$this->start_controls_tab(
+				'ma_el_infobox_hover',
+				[
+					'label'                 => __( 'Hover', MELA_TD ),
+				]
+			);
+
+			$this->add_group_control(
+				Group_Control_Background::get_type(),
+				[
+					'name' => 'ma_el_infobox_hover_bg',
+					'label' => __( 'Background Color', MELA_TD ),
+					'types' => [ 'classic', 'gradient' ],
+					'selectors'	=> [
+						'{{WRAPPER}} .ma-el-infobox.one .ma-el-infobox-item:hover',
+					],
+					'default' => '#FFFFFF',
+				]
+			);
+			$this->end_controls_tab();
+
+			$this->end_controls_tabs();
+
+
+			$this->add_control(
+				'ma_el_infobox_hover_animation',
+				[
+					'label' => __( 'Hover Animation', MELA_TD ),
+					'type' => Controls_Manager::HOVER_ANIMATION,
+					'separator' => 'before',
+					'prefix_class' => 'elementor-animation-',
+//					'selector'	=> '.ma-el-infobox-item',
+
 				]
 			);
 
 
 			$this->end_controls_section();
+
+
+
 
 			// Title , Description Font Color and Typography
 
@@ -222,6 +326,17 @@
 					'tab' => Controls_Manager::TAB_STYLE,
 				]
 			);
+
+
+			$this->start_controls_tabs( 'ma_el_infobox_title_color_style' );
+
+			$this->start_controls_tab(
+				'ma_el_infobox_title_color_normal',
+				[
+					'label'                 => __( 'Normal', MELA_TD ),
+				]
+			);
+
 
 			$this->add_control(
 				'ma_el_title_color',
@@ -235,22 +350,64 @@
 				]
 			);
 
-			$this->add_group_control(
-				Group_Control_Typography::get_type(),
+			$this->end_controls_tab();
+
+			$this->start_controls_tab(
+				'ma_el_infobox_title_hover',
 				[
-					'name' => 'infobox_title_typography',
-					'selector' => '{{WRAPPER}} .ma-el-infobox-content-title',
+					'label'                 => __( 'Hover', MELA_TD ),
 				]
 			);
+
+
+			$this->add_control(
+				'ma_el_infobox_title_color_hover',
+				[
+					'label' => __('Color', MELA_TD),
+					'type' => Controls_Manager::COLOR,
+					'default' => '#132c47',
+					'selectors' => [
+						'{{WRAPPER}} .ma-el-infobox-content-title:hover' => 'color: {{VALUE}};',
+					],
+				]
+			);
+
+			$this->end_controls_tab();
+
+			$this->end_controls_tabs();
+
 
 			$this->end_controls_section();
 
 
+
+
+
+			// Description Style
 			$this->start_controls_section(
 				'section_infobox_description',
 				[
 					'label' => __('Description', MELA_TD),
 					'tab' => Controls_Manager::TAB_STYLE,
+				]
+			);
+
+
+			$this->add_group_control(
+				Group_Control_Typography::get_type(),
+				[
+					'name' => 'ma_el_description_typography',
+					'selector' => '{{WRAPPER}} .ma-el-infobox-content-description',
+				]
+			);
+
+			
+			$this->start_controls_tabs( 'ma_el_infobox_desc_color_style' );
+
+			$this->start_controls_tab(
+				'ma_el_infobox_desc_color_normal',
+				[
+					'label'                 => __( 'Normal', MELA_TD ),
 				]
 			);
 
@@ -265,20 +422,50 @@
 					],
 				]
 			);
+			$this->end_controls_tab();
 
-			$this->add_group_control(
-				Group_Control_Typography::get_type(),
+			$this->start_controls_tab(
+				'ma_el_infobox_desc_hover',
 				[
-					'name' => 'ma_el_description_typography',
-					'selector' => '{{WRAPPER}} .ma-el-infobox-content-description',
+					'label'                 => __( 'Hover', MELA_TD ),
 				]
 			);
 
+
+			$this->add_control(
+				'ma_el_infobox_desc_color_hover',
+				[
+					'label' => __('Color', MELA_TD),
+					'type' => Controls_Manager::COLOR,
+					'default' => '#132c47',
+					'selectors' => [
+						'{{WRAPPER}} .ma-el-infobox-content-description:hover' => 'color: {{VALUE}};',
+					],
+				]
+			);
+
+			$this->end_controls_tab();
+
+			$this->end_controls_tabs();
+
 			$this->end_controls_section();
+
+
 
 		}
 		protected function render() {
 			$settings = $this->get_settings_for_display();
+
+
+			// Demo Link
+			if( $settings['ma_el_infobox_readmore_link']['is_external'] ) {
+				$this->add_render_attribute( 'ma_el_infobox_readmore', 'target', '_blank' );
+			}
+
+			if( $settings['ma_el_infobox_readmore_link']['nofollow'] ) {
+				$this->add_render_attribute( 'ma_el_infobox_readmore', 'rel', 'nofollow' );
+			}
+
 
 			$infobox_image = $this->get_settings_for_display( 'ma_el_infobox_image' );
 			$infobox_image_url = Group_Control_Image_Size::get_attachment_image_src( $infobox_image['id'], 'thumbnail', $settings );
@@ -296,32 +483,68 @@
                 <div class="ma-el-infobox-item">
 
 					<?php if( $settings['ma_el_infobox_img_or_icon'] != 'none' ) : ?>
+
+						<?php if($settings['ma_el_infobox_preset']=="two"){ ?>
+                            <div class="bg-fade-icon">
+                                <i class="<?php echo esc_attr( $settings['ma_el_infobox_icon'] ); ?>"></i>
+                            </div>
+						<?php }?>
+
+						<?php if($settings['ma_el_infobox_preset']=="three"){ ?>
+                            <div class="bg-fade-icon">
+                                <i class="<?php echo esc_attr( $settings['ma_el_infobox_icon'] ); ?>"></i>
+                            </div>
+
+						<?php }?>
+
+
+
                         <div class="ma-el-infobox-icon">
+	                        <?php if( $settings['ma_el_infobox_preset']=="five"){ ?>
+                                <i class="<?php echo esc_attr( $settings['ma_el_infobox_icon'] ); ?>"></i>
+	                        <?php } ?>
+
                             <div class="inner-content">
 
-								<?php if( 'icon' == $settings['ma_el_infobox_img_or_icon'] ) : ?>
+								<?php if( 'icon' == $settings['ma_el_infobox_img_or_icon'] || 'imgicon' == $settings['ma_el_infobox_img_or_icon'] ) : ?>
                                     <i class="<?php echo esc_attr( $settings['ma_el_infobox_icon'] ); ?>"></i>
 								<?php endif; ?>
 
-								<?php if( 'img' == $settings['ma_el_infobox_img_or_icon'] ) : ?>
-                                    <img src="<?php echo esc_url( $infobox_image_url ); ?>" alt="Icon Image">
+								<?php if( 'img' == $settings['ma_el_infobox_img_or_icon'] || 'imgicon' == $settings['ma_el_infobox_img_or_icon'] ) : ?>
+                                    <img src="<?php echo esc_url( $infobox_image_url ); ?>" alt="<?php echo get_post_meta( $infobox_image['id'], '_wp_attachment_image_alt', true);?>">
 								<?php endif; ?>
 
-								<?php if($settings['ma_el_infobox_preset']=="ten"){ ?>
+	                            <?php if( $settings['ma_el_infobox_preset']=="seven"){ ?>
                                     <h3 class="ma-el-infobox-content-title"><?php echo $settings['ma_el_infobox_title']; ?></h3>
-								<?php }?>
+	                            <?php } ?>
+
+	                            <?php if( $settings['ma_el_infobox_preset']=="nine"){ ?>
+                                    <h3 class="ma-el-infobox-content-title"><?php echo $settings['ma_el_infobox_title']; ?></h3>
+	                            <?php } ?>
+
+	                            <?php if($settings['ma_el_infobox_preset']=="five"){ ?>
+                                    <div class="hexagon-shape"><div class="shape-inner"></div></div>
+	                            <?php }?>
                             </div><!-- /.inner-content -->
                         </div>
 					<?php endif; ?>
 
                     <div class="ma-el-infobox-content">
                         <div class="inner-content">
-							<?php if($settings['ma_el_infobox_preset']!=="ten"){ ?>
+							<?php if($settings['ma_el_infobox_preset']!=="seven"){ ?>
                                 <h3 class="ma-el-infobox-content-title"><?php echo $settings['ma_el_infobox_title']; ?></h3>
 							<?php }?>
                             <p class="ma-el-infobox-content-description">
 								<?php echo $settings['ma_el_infobox_description']; ?>
                             </p>
+
+	                        <?php if($settings['ma_el_infobox_preset']=="six"){ ?>
+                                <a href="<?php echo esc_url_raw( $settings['ma_el_infobox_readmore_link']['url'] );?>" class="ma-el-btn-learn"
+	                                <?php echo $this->get_render_attribute_string( 'ma_el_infobox_readmore' ); ?>>
+	                                <?php echo esc_html($settings['ma_el_infobox_readmore_text']);?>
+                                    <i class="ti-arrow-right"></i>
+                                </a>
+	                        <?php }?>
                         </div><!-- /.inner-content -->
                     </div>
                 </div>
