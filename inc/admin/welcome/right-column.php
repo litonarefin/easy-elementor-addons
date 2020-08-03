a4610f0b0287c99b45029e4027d5830bafa127cf
litonarefin <litonice09@gmail.com>
2020-08-03 22:29:58 +0600

    changelogs right sidebar style fixed

--- inc/admin/welcome/right-column.php
+++ inc/admin/welcome/right-column.php
@@ -7,16 +7,16 @@
             <li class="col">
                 <h3 class="sidebar_title">
                     <span class="dashicons dashicons-download"></span>
-					<?php echo esc_html__('Free Theme Download', MELA_TD);?>
+					<?php echo esc_html__('Download Free Theme', MELA_TD);?>
                 </h3>
                 <div class="inner_content">
-                    <a class="master_addons_theme_link" href="<?php echo esc_url( 'https://wordpress.org/themes/osaka-light/' ); ?>" target="_blank">
-                        <img src="https://master-addons.com/wp-content/uploads/2019/08/osaka-light.png" alt="Master
+                    <a class="master_addons_theme_link" href="<?php echo esc_url( 'https://wordpress.org/themes/polmo-lite/' ); ?>" target="_blank">
+                        <img src="https://master-addons.com/wp-content/uploads/2019/08/polmo-lite.png" alt="Master
                         Addons for Elementor
                         Theme">
                     </a>
                     <p>
-						<?php echo esc_html__('Download FREE "Osaka Lite" WordPress Theme now to get started with "Master Addons for Elementor" Plugin.', MELA_TD);?>
+						<?php echo esc_html__('Download FREE "Polmo Lite" WordPress Theme now to get started with "Master Addons for Elementor" Plugin.', MELA_TD);?>
                     </p>
                     <a class="button button-primary master_addons_button" href="<?php echo esc_url( 'https://wordpress.org/themes/polmo-lite/' ); ?>" target="_blank">
 						<?php _e( 'Download' ); ?>
@@ -31,38 +31,10 @@
                 </h3>
                 <div class="inner_content">
                     <p>
-						<?php echo esc_html__('Get Updates and Stay Connected with Master Addons for Elementor Plugin. We\'ll notify you when new Blocks/Updates will be released.', MELA_TD);?>
+						<?php echo esc_html__('Get Updates and Stay Connected with "Master Addons for Elementor" Plugin. We\'ll notify you when Updates will be released.', MELA_TD);?>
                     </p>
                     <a class="button button-primary master_addons_button" href="<?php echo esc_url( 'https://jeweltheme.com/newsletters/' ); ?>" target="_blank">
 						<?php _e( 'Subscribe' ); ?>
-                    </a>
-                </div>
-            </li>
-            <li class="col">
-                <h3 class="sidebar_title">
-                    <span class="dashicons dashicons-hammer"></span>
-					<?php echo esc_html__('Gutenberg Tutorials', MELA_TD);?>
-                </h3>
-                <div class="inner_content">
-                    <p>
-						<?php echo esc_html__('Check out Latest Tips and Tricks on Gutenberg Editor. It will help you with some hack on Gutenberg Editor.', MELA_TD);?>
-                    </p>
-                    <a class="button button-primary master_addons_button" href="<?php echo esc_url( 'https://jeweltheme.com/category/tutorials/gutenberg/' ); ?>" target="_blank">
-						<?php _e( 'Gutenberg Tutorials' ); ?>
-                    </a>
-                </div>
-            </li>
-            <li class="col">
-                <h3 class="sidebar_title">
-                    <span class="dashicons dashicons-edit"></span>
-					<?php echo esc_html__('Bug Report and Suggestions', MELA_TD);?>
-                </h3>
-                <div class="inner_content">
-                    <p>
-						<?php echo esc_html__('Gutenberg is changing every day. We always try to be updated. If you find some kind of bug as well as suggestions, please notify us.', MELA_TD);?>
-                    </p>
-                    <a class="button button-primary master_addons_button" href="<?php echo esc_url( 'https://jeweltheme.com/category/tutorials/gutenberg/' ); ?>" target="_blank">
-						<?php _e( 'Support Forum' ); ?>
                     </a>
                 </div>
             </li>
