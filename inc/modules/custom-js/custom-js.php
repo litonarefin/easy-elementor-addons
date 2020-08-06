3dd658459721ccecccf89f6f3c5c13197c6e1b0a
litonarefin <litonice09@gmail.com>
2020-08-07 02:47:18 +0600

    fixed Custom JS codes

--- inc/modules/custom-js/custom-js.php
+++ inc/modules/custom-js/custom-js.php
@@ -6,7 +6,7 @@ use Elementor\Controls_Manager;
 /**
  * Author Name: Liton Arefin
  * Author URL: https://jeweltheme.com
- * Date: 07/08/20
+ * Date: 04/08/20
  */
 if ( ! defined( 'ABSPATH' ) ) { exit; } // Exit if accessed directly.
 
@@ -23,43 +23,43 @@ class Master_Addons_Custom_JS{
         $controls->start_controls_section(
             'jtlma_section_custom_js',
             [
-                'label'     => MA_EL_BADGE . __( ' Custom JS', MELA_TD ),
-                'tab'       => Controls_Manager::TAB_ADVANCED,
+                'label'         => MA_EL_BADGE . __( ' Custom JS', MELA_TD ),
+                'tab'           => Controls_Manager::TAB_ADVANCED,
             ]
         );
 
         $controls->add_control(
             'jtlma_custom_js_label',
             [
-                'type'      => Controls_Manager::RAW_HTML,
-                'raw'       => __('Add your own custom JS here', MELA_TD),
+                'type'          => Controls_Manager::RAW_HTML,
+                'raw'           => __('Add your own custom JS here', MELA_TD),
             ]
         );
 
         $controls->add_control(
             'jtlma_custom_js',
             [
-                'type' => Controls_Manager::CODE,
-                'show_label' => false,
-                'language' => 'javascript',
+                'type'          => Controls_Manager::CODE,
+                'show_label'    => false,
+                'language'      => 'javascript',
             ]
         );
 
         $controls->add_control(
             'jtlma_custom_js_usage',
             [
-                'type' => Controls_Manager::RAW_HTML,
-                'raw' => __('You may use both jQuery selector e.g. $(‘.selector’) or Vanilla JS selector e.g. document.queryselector(‘.selector’)', MELA_TD),
-                'content_classes' => 'elementor-descriptor',
+                'type'              => Controls_Manager::RAW_HTML,
+                'raw'               => __('You may use both jQuery selector e.g. $(‘.selector’) or Vanilla JS selector e.g. document.queryselector(‘.selector’)', MELA_TD),
+                'content_classes'   => 'elementor-descriptor',
             ]
         );
 
         $controls->add_control(
             'jtlma_custom_js_docs',
             [
-                'type' => Controls_Manager::RAW_HTML,
-                'raw' => __('For more information, <a href="https://master-addons.com/docs/addons/custom-js-extension/" target="_blank">click here</a>', MELA_TD),
-                'content_classes' => 'elementor-descriptor',
+                'type'              => Controls_Manager::RAW_HTML,
+                'raw'               => __('For more information, <a href="https://master-addons.com/docs/addons/custom-js-extension/" target="_blank">click here</a>', MELA_TD),
+                'content_classes'   => 'elementor-descriptor',
             ]
         );
 
