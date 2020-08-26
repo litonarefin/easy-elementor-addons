94eb5b1806d6f389dc5a9369e45a5a6de42ed904
litonarefin <litonice09@gmail.com>
2020-08-26 18:27:00 +0600

    admin dashboard not loading issue fixed

--- inc/admin/welcome/third-party-plugins.php
+++ inc/admin/welcome/third-party-plugins.php
@@ -60,7 +60,7 @@
 									if ( ! current_user_can( 'install_plugins' ) ) { return; }
 									if( !jltma_is_plugin_active( $plugin_file ) ){
 								        $activation_url = wp_nonce_url( 'plugins.php?action=activate&amp;plugin=' . $plugin_file . '&amp;plugin_status=all&amp;paged=1&amp;s', 'activate-plugin_' . $plugin_file );
-					                    $html = '<a class="thrd-party-plgin-dnld thrd-party-plgin-dnld-active" href="' . $activation_url . '" ><span class="thrd-party-plgin-dnld thrd-party-plgin-dnld-active pr-1">' . esc_html__('Activate' MELA_TD) . '</span><i class="dashicons dashicons-yes-alt"></i></a>';
+					                    $html = '<a class="thrd-party-plgin-dnld thrd-party-plgin-dnld-active" href="' . $activation_url . '" ><span class="thrd-party-plgin-dnld thrd-party-plgin-dnld-active pr-1">' . esc_html__('Activate', MELA_TD) . '</span><i class="dashicons dashicons-yes-alt"></i></a>';
 		        					}else{
 		        						$html ='';
 		        					}
@@ -68,7 +68,7 @@
 		        				} else{
 
 	        						$install_url = wp_nonce_url( self_admin_url( 'update.php?action=install-plugin&plugin=' . $plugin_slug ), 'install-plugin_' . $plugin_slug );
-		                    		$html = '<a class="thrd-party-plgin-dnld" href="' . $install_url . '"><span class="thrd-party-plgin-dnld pr-1">' . esc_html__('Download' MELA_TD) . '</span><i class="dashicons dashicons-download"></i></a>';
+		                    		$html = '<a class="thrd-party-plgin-dnld" href="' . $install_url . '"><span class="thrd-party-plgin-dnld pr-1">' . esc_html__('Download', MELA_TD) . '</span><i class="dashicons dashicons-download"></i></a>';
 
 		                    		activate_plugin($plugin_file);
 		        					
