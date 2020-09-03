95410578fb965a1f60a7ebfc84dd3b699d4e5a4f
litonarefin <litonice09@gmail.com>
2020-09-04 02:18:41 +0600

    breakpoints assets added

--- inc/admin/assets/js/editor.js
+++ inc/admin/assets/js/editor.js
@@ -556,10 +556,8 @@
         } );
 
         elementor.addControlView( 'jltma_query', JLTMA_ControlQuery );
-
-
-    
-
 	} );
+
+    "use strict";!function(n){var t=function(){function n(){this.document=elementor.documents.currentDocument,this.breakpoints=elementorFrontend.config.breakpoints,this.init()}var t=n.prototype;return t.init=function(){var n=this;this.document.container.children.forEach(function(t){n.updateBreakPointCSS(t)}),elementor.hooks.addAction("panel/open_editor/widget",function(t,e,o){n.delay(function(){n.addDevice(o)})}),elementor.hooks.addAction("panel/open_editor/section",function(t,e,o){n.delay(function(){n.runFromRootSection(o)})}),elementor.hooks.addAction("panel/open_editor/column",function(t,e,o){n.delay(function(){n.runFromRootSection(o)})})},t.delay=function(n,t,e){void 0===t&&(t=10),void 0===e&&(e=20);var o=setInterval(function(){n(),0>=e&&clearInterval(o)},t)},t.runFromRootSection=function(n){var t=this.getRootSection(n.container);t&&this.updateBreakPointCSS(t)},t.updateBreakPointCSS=function(n){var t=this;n.view&&this.addDevice(n.view),n.children.forEach(function(n){t.updateBreakPointCSS(n)})},t.getRootSection=function(n){return n.parent||console.log("Something went wrong"),n.parent&&"document"==n.parent.type&&"section"==n.type?n:this.getRootSection(n.parent)},t.addDevice=function(n){var t=n.controlsCSSParser.stylesheet;for(var e in this.breakpoints)if(!["xs","sm","md","lg","xxl"].includes(e)){var o=this.breakpoints[e].input1;void 0===o&&(o=this.breakpoints[e]),t.addDevice(e,o)}this.renderStyles(n)},t.renderStyles=function(n){n.renderStyles()},n}();n(window).load(function(){new t})}(jQuery);
 
 })(jQuery, window, document);
