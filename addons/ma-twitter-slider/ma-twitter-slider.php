f9981ac0638da16c9d7a0f8fa16b36ae7bddcb70
litonarefin <litonice09@gmail.com>
2020-09-23 19:57:23 +0600

    twitter slider

--- addons/ma-twitter-slider/ma-twitter-slider.php
+++ addons/ma-twitter-slider/ma-twitter-slider.php
@@ -3,9 +3,14 @@ namespace Elementor;
 
 // Elementor Classes
 use Elementor\Widget_Base;
+use Elementor\Controls_Manager;
+use Elementor\Group_Control_Image_Size;
+use Elementor\Group_Control_Border;
 use Elementor\Group_Control_Typography;
-use Elementor\Scheme_Color;
-use Elementor\Scheme_Typography;    
+use Elementor\Scheme_Typography;
+use Elementor\Group_Control_Box_Shadow;
+
+use MasterAddons\Inc\Helper\Master_Addons_Helper;
 
 /**
  * Author Name: Liton Arefin
@@ -72,12 +77,32 @@ class Master_Addons_Twitter_Slider extends Widget_Base {
         * Content: Layout
         */
         $this->start_controls_section(
-            'jltma_ts_layout',
+            'jltma_ts_section_layout',
             [
                 'label' => esc_html__( 'Layout', MELA_TD ),
             ]
         );
 
+
+        $this->add_responsive_control(
+            'jltma_ts_columns',
+            [
+                'label'          => esc_html__( 'Columns', MELA_TD ),
+                'type'           => Controls_Manager::SELECT,
+                'default'        => '3',
+                'tablet_default' => '2',
+                'mobile_default' => '1',
+                'options'        => [
+                    '1' => '1',
+                    '2' => '2',
+                    '3' => '3',
+                    '4' => '4',
+                    '5' => '5',
+                    '6' => '6',
+                ],
+            ]
+        );
+        
         $this->add_control(
             'jltma_ts_tweet_num',
             [
@@ -191,7 +216,7 @@ class Master_Addons_Twitter_Slider extends Widget_Base {
                 'label'     => esc_html__( 'Arrows and Dots Position', MELA_TD ),
                 'type'      => Controls_Manager::SELECT,
                 'default'   => 'center',
-                'options'   => element_pack_navigation_position(),
+                'options'   => Master_Addons_Helper::jltma_carousel_navigation_position(),
                 'condition' => [
                     'jltma_ts_navigation' => 'both',
                 ],
@@ -204,7 +229,7 @@ class Master_Addons_Twitter_Slider extends Widget_Base {
                 'label'     => esc_html__( 'Arrows Position', MELA_TD ),
                 'type'      => Controls_Manager::SELECT,
                 'default'   => 'center',
-                'options'   => element_pack_navigation_position(),
+                'options'   => Master_Addons_Helper::jltma_carousel_navigation_position(),
                 'condition' => [
                     'jltma_ts_navigation' => 'arrows',
                 ],              
@@ -217,7 +242,7 @@ class Master_Addons_Twitter_Slider extends Widget_Base {
                 'label'     => esc_html__( 'Dots Position', MELA_TD ),
                 'type'      => Controls_Manager::SELECT,
                 'default'   => 'bottom-center',
-                'options'   => element_pack_pagination_position(),
+                'options'   => Master_Addons_Helper::jltma_carousel_pagination_position(),
                 'condition' => [
                     'jltma_ts_navigation' => 'dots',
                 ],              
@@ -228,7 +253,149 @@ class Master_Addons_Twitter_Slider extends Widget_Base {
 
 
         /*
-        * Content: Layout
+        * Content: Slider Setting
+        */
+
+        $this->start_controls_section(
+            'jltma_ts_section_slider_settins',
+            [
+                'label' => esc_html__( 'Slider Settings', MELA_TD ),
+            ]
+        );
+
+
+        $this->add_control(
+            'jltma_ts_autoplay',
+            [
+                'label'   => esc_html__( 'Auto Play', MELA_TD ),
+                'type'    => Controls_Manager::SWITCHER,
+                'default' => 'yes',
+            ]
+        );
+
+        $this->add_control(
+            'jltma_ts_autoplay_speed',
+            [
+                'label'     => esc_html__( 'Autoplay Speed', MELA_TD ),
+                'type'      => Controls_Manager::NUMBER,
+                'default'   => 5000,
+                'condition' => [
+                    'jltma_ts_autoplay' => 'yes',
+                ],
+            ]
+        );
+
+        $this->add_control(
+            'jltma_ts_pauseonhover',
+            [
+                'label' => esc_html__( 'Pause on Hover', MELA_TD ),
+                'type'  => Controls_Manager::SWITCHER,
+            ]
+        );
+
+        $this->add_control(
+            'jltma_ts_speed',
+            [
+                'label'   => __( 'Animation Speed (ms)', MELA_TD ),
+                'type'    => Controls_Manager::SLIDER,
+                'default' => [
+                    'size' => 500,
+                ],
+                'range' => [
+                    'min'  => 100,
+                    'max'  => 5000,
+                    'step' => 50,
+                ],
+            ]
+        );
+
+        $this->add_control(
+            'jltma_ts_loop',
+            [
+                'label'   => esc_html__( 'Loop', MELA_TD ),
+                'type'    => Controls_Manager::SWITCHER,
+                'default' => 'yes',
+            ]
+        );
+
+        $this->add_control(
+            'jltma_ts_transition',
+            [
+                'label'   => esc_html__( 'Transition', MELA_TD ),
+                'type'    => Controls_Manager::SELECT,
+                'default' => 'slide',
+                'options' => [
+                    'slide'     => esc_html__( 'Slide', MELA_TD ),
+                    'fade'      => esc_html__( 'Fade', MELA_TD ),
+                    'cube'      => esc_html__( 'Cube', MELA_TD ),
+                    'coverflow' => esc_html__( 'Coverflow', MELA_TD ),
+                    'flip'      => esc_html__( 'Flip', MELA_TD ),
+                ],
+            ]
+        );        
+        $this->end_controls_section();
+
+
+
+
+        /*
+        * STYLE: Items
+        */
+
+        $this->start_controls_section(
+            'jltma_ts_section_style_layout',
+            [
+                'label' => __( 'Items', MELA_TD ),
+                'tab'   => Controls_Manager::TAB_STYLE,
+            ]
+        );
+
+        $this->add_control(
+            'jltma_ts_item_color',
+            [
+                'label'     => __( 'Color', MELA_TD ),
+                'type'      => Controls_Manager::COLOR,
+                'selectors' => [
+                    '{{WRAPPER}} .bdt-twitter-slider .bdt-carousel-item .bdt-twitter-text,
+                    {{WRAPPER}} .bdt-twitter-slider .bdt-carousel-item .bdt-twitter-text *' => 'color: {{VALUE}};',
+                ],
+            ]
+        );
+
+        $this->add_control(
+            'jltma_ts_alignment',
+            [
+                'label'   => __( 'Alignment', MELA_TD ),
+                'type'    => Controls_Manager::CHOOSE,
+                'default' => 'center',
+                'options' => [
+                    'left'    => [
+                        'title' => __( 'Left', MELA_TD ),
+                        'icon'  => 'fa fa-align-left',
+                    ],
+                    'center' => [
+                        'title' => __( 'Center', MELA_TD ),
+                        'icon'  => 'fa fa-align-center',
+                    ],
+                    'right' => [
+                        'title' => __( 'Right', MELA_TD ),
+                        'icon'  => 'fa fa-align-right',
+                    ],
+                ],
+                'selectors' => [
+                    '{{WRAPPER}} .bdt-twitter-slider .bdt-carousel-item .bdt-card-body' => 'text-align: {{VALUE}};',
+                ],
+            ]
+        );
+
+        $this->end_controls_section();
+
+        /*
+        * STYLE: Items
+        */
+
+        /*
+        * STYLE: Slider Setting
         */
 
 	}
