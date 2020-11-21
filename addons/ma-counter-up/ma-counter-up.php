cab87d84a70bb6e034468d64b79e7567c8101e61
litonarefin <litonice09@gmail.com>
2020-11-21 22:33:43 +0600

    counter up

--- addons/ma-counter-up/ma-counter-up.php
+++ addons/ma-counter-up/ma-counter-up.php
@@ -25,11 +25,6 @@ if ( ! defined( 'ABSPATH' ) ) exit; // If this file is called directly, abort.
 
 class Master_Addons_Counter_Up extends Widget_Base {
 
-    public function __construct( $data = [], $args = null ) {
-		parent::__construct( $data, $args );
-		$this->add_script_depends('ma-counter-up');
-	}
-
 	//use ElementsCommonFunctions;
 	public function get_name() {
 		return 'jltma-counter-up';
