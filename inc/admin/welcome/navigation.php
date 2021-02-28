7176ab8676c2936d5d0934a1b694707ca12124ab
litonarefin <litonice09@gmail.com>
2021-02-28 09:04:43 +0600

    working on white label

--- inc/admin/welcome/navigation.php
+++ inc/admin/welcome/navigation.php
@@ -1,54 +1,54 @@
-
 <!-- Start tabs -->
 <ul class="wp-tab-bar master_addons_navbar">
 
 	<li class="wp-tab-active">
 		<a href="#welcome">
-			<?php _e( 'Welcome', MELA_TD ); ?>
+			<?php _e('Welcome', MELA_TD); ?>
 		</a>
 	</li>
 
-    <li>
-        <a href="#ma-addons">
-			<?php _e( 'Addons', MELA_TD ); ?>
-        </a>
-    </li>
+	<li>
+		<a href="#ma-addons">
+			<?php _e('Addons', MELA_TD); ?>
+		</a>
+	</li>
 
 	<li>
 		<a href="#extensions">
-			<?php _e( 'Extensions', MELA_TD ); ?>
+			<?php _e('Extensions', MELA_TD); ?>
 		</a>
 	</li>
 
 	<li>
 		<a href="#ma_api_keys">
-			<?php _e( 'API', MELA_TD ); ?>
+			<?php _e('API', MELA_TD); ?>
 		</a>
-	</li> 
-	
-	<?php //if ( ma_el_fs()->is_plan('developer') ) { ?>
-		<!-- <li>
-			<a href="#white-labeling">
-				<?php //_e( 'Branding', MELA_TD ); ?>
-			</a>
-		</li> -->
-	<?php //} ?>
+	</li>
+
+
+	<li>
+		<a href="#white-label">
+			<?php _e('White Label', MELA_TD);
+			?>
+		</a>
+	</li>
+
 
 	<li>
 		<a href="#version">
-			<?php _e( 'Version', MELA_TD ); ?>
+			<?php _e('Version', MELA_TD); ?>
 		</a>
 	</li>
 
 	<li>
 		<a href="#changelogs">
-			<?php _e( 'Changelogs', MELA_TD ); ?>
+			<?php _e('Changelogs', MELA_TD); ?>
 		</a>
 	</li>
 
-	<?php if ( ma_el_fs()->is_not_paying() ) { ?>
+	<?php if (ma_el_fs()->is_not_paying()) { ?>
 		<a class="nav-upgrade-pro" href="https://master-addons.com/pricing" target="_blank">
-			<?php _e( 'Upgrade Pro', MELA_TD ); ?>
+			<?php _e('Upgrade Pro', MELA_TD); ?>
 		</a>
 	<?php } ?>
 
